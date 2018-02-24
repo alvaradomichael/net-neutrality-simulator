005bde135794e4c7e3af035a9d421328623fb476
Michael <michaelalvarado@uagateway.org>
2018-02-24 14:20:20 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,17 @@
+window.alert('Save net neutraility')
+document.querySelector('p')
+
+
+document.querySelector('button').addEventListener('click', function() {
+
+document.querySelector('body').style.background = 'red';
+document.querySelector('button').style.color = '';
+document.querySelector('h2').textContent = 'Loading...'
+document.querySelector('p').textContent = "";
+});
+
+
+document.querySelector('button').addEventListener('click', function() {
+var img = document.querySelectorAll('img')
+img.src = 'https://media1.giphy.com'
+});
